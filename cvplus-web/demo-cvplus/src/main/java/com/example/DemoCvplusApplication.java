11a6d8465db9f469fce04691eaf87a1d5766d053
Carlos Ramos R <carlos.rr2204@gmail.com>
2022-06-26 14:37:25 -0500

    Inicio del proyecto

---
+++ cvplus-web/demo-cvplus/src/main/java/com/example/DemoCvplusApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCvplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCvplusApplication.class, args);
+	}
+
+}
