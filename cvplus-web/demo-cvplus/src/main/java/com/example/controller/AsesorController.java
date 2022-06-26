c260a62f98887c4cd55a741aaf607eb05070f94f
Carlos Ramos R <carlos.rr2204@gmail.com>
2022-06-26 17:35:50 -0500

    Implemento de las vistas de listado y busqueda de Asesores

--- cvplus-web/demo-cvplus/src/main/java/com/example/controller/AsesorController.java
+++ cvplus-web/demo-cvplus/src/main/java/com/example/controller/AsesorController.java
@@ -2,9 +2,15 @@ package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entities.Asesor;
 import com.example.service.AsesorService;
 import com.example.service.RubroService;
 
@@ -18,8 +24,47 @@ public class AsesorController {
 	@Autowired
 	private RubroService rubroService;
 
+	@GetMapping("/nuevo")
+	public String registrarAsesor(Model model) {
+		model.addAttribute("asesor", new Asesor());
+		model.addAttribute("rubros", rubroService.listarRubros());
+		return "asesor/form";
+	}
+
 	@GetMapping
-	public String listarAsesores() {
+	public String listarAsesores(Model model) {
+		model.addAttribute("asesor", new Asesor());
+		model.addAttribute("asesores", asesorService.listarAsesores());
+		return "asesor/list";
+	}
+
+	@PostMapping("/registrar")
+	public String registrarAsesor(@Validated @ModelAttribute Asesor asesor, BindingResult result, Model model) {
+		int rpta;
+
+		if (result.hasErrors()) {
+			model.addAttribute("rubros", rubroService.listarRubros());
+			return "asesor/form";
+		}
+
+		rpta = asesorService.registrarAsesor(asesor);
+
+		if (rpta > 0) {
+			model.addAttribute("mensaje", "El numero de dni ya existe");
+			model.addAttribute("rubros", rubroService.listarRubros());
+
+		} else {
+			model.addAttribute("mensaje", "Se registro nuevo asesor");
+			model.addAttribute("asesor", new Asesor());
+			model.addAttribute("rubros", rubroService.listarRubros());
+		}
+
+		return "asesor/form";
+	}
+
+	@PostMapping("/buscar")
+	public String buscarAsesor(Model model, @ModelAttribute Asesor asesor) {
+		model.addAttribute("asesores", asesorService.buscarAsesorPorDni(asesor.getDni()));
 		return "asesor/list";
 	}
 
